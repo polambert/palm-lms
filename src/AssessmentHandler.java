cacf3721c7625a62e4829ddced287fbf7c5f76a3
Ayush Patel <ayushpatel@Ayushs-MacBook-Pro.local>
2023-03-14 12:20:08 -0400

    Assessment Handler g and s

--- src/AssessmentHandler.java
+++ src/AssessmentHandler.java
@@ -6,6 +6,47 @@ public class AssessmentHandler
     private Question currentQuestion;
     private int currentQuestionNumber;
 
+    //getters and setters
+    public Assessment getAssessment() {
+        return assessment;
+    }
+
+    public void setAssessment(Assessment assessment) {
+        this.assessment = assessment;
+    }
+
+    public int getNumberCorrect() {
+        return numberCorrect;
+    }
+
+    public void setNumberCorrect(int numberCorrect) {
+        this.numberCorrect = numberCorrect;
+    }
+
+    public int getNumberIncorrect() {
+        return numberIncorrect;
+    }
+    public void setNumberIncorrect(int numberIncorrect) {
+        this.numberIncorrect = numberIncorrect;
+    }
+
+    public Question getCurrentQuestion() {
+        return currentQuestion;
+    }
+
+    public void setCurrentQuestion(Question currentQuestion) {
+        this.currentQuestion = currentQuestion;
+    }
+
+    public int getCurrentQuestionNumber() {
+        return currentQuestionNumber;
+    }
+
+    public void setCurrentQuestionNumber(int currentQuestionNumber) {
+        this.currentQuestionNumber = currentQuestionNumber;
+    }
+
+
     public AssessmentHandler(){
 
     }
