4765891711d29e8a4a22666f22f506d0d67d6867
Ayush Patel <ayushpatel@Ayushs-MacBook-Pro.local>
2023-03-13 20:03:34 -0400

    Course and UIHandler stubs

---
+++ src/Course.java
@@ -0,0 +1,42 @@
+package 247.palm-lms.src;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Course 
+{
+    private UUID id;
+    private String language;
+    private String title;
+    private String name;
+    private User author;
+    private String description;
+    private ArrayList<Chapter> chapters = new ArrayList<>();
+    private Assessment finalExam;
+    private ArrayList<Review> reviews = new ArrayList<>();
+    private ArrayList<Comment> comments = new ArrayList<>();
+
+    //create
+    public Course(int id, String title)
+    {
+        this.id = UUID.randomUUID();
+        this.title = title;
+    }
+
+
+    public Review addReview(int rating, String text, User author)
+    {
+        return review;
+    }
+
+    public int getChapterCount()
+    {
+        return int;
+    }
+    
+    public Assessment getFinalExam()
+    {
+        return assessment;
+    }
+
+
--- src/UIHandler.java
+++ src/UIHandler.java
@@ -1,3 +1,68 @@
+public class UIHandler
+{
+    private ProgramState state;
+    private AssessmentHandler assessmentHandler;
+    private CourseManager courseManager;
+    private UserManager UserManager;
 
-// TODO: Write all method stubs
+    public void main(String args[])
+    {
+
+    }
+
+    public boolean login(String email, char[] password)
+    {
+        return true;
+    }
+
+    public boolean logout()
+    {
+        return true;
+    }
+
+    public boolean signup(String name, String email, Date dob, char[] password)
+    {
+        return true;
+    }
+
+    public void startAssessment(CourseProgress courseProgress)
+    {
+
+    }
+
+    public boolean enrollCourse(Course course)
+    {
+        return true;
+    }
+
+    public boolean dropCourse(Course course)
+    {
+        return true;
+    }
+
+    public boolean makeCourse()
+    {
+        return true;
+    }
+
+    public boolean leaveReview(Review review, Course course)
+    {
+        return true;
+    }
+
+    public boolean leaveComment(Comment comment, Course course)
+    {
+        return true;
+    }
+
+    public boolean deleteReview(Review review, Course course)
+    {
+        return true;
+    }
+
+    public boolean deleteComment(Comment comment, Course course)
+    {
+        return true;
+    }   
+}
 
