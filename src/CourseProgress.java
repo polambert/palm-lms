663a63072e4dad00adefb755bcdf76209d6fb4f1
Ayush Patel <ayushpatel@Ayushs-MacBook-Pro.local>
2023-03-14 12:10:44 -0400

    Course Progress g and c

--- src/CourseProgress.java
+++ src/CourseProgress.java
@@ -14,13 +14,69 @@ public class CourseProgress
 
 
     // getters and setters
+    public Course getCourse() {
+        return course;
+    }
 
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 
+    public User getUser() {
+        return user;
+    }
 
+    public void setUser(User user) {
+        this.user = user;
+    }
 
+    public int getChaptersCompleted() {
+        return chaptersCompleted;
+    }
 
+    public void setChaptersCompleted(int chaptersCompleted) {
+        this.chaptersCompleted = chaptersCompleted;
+    }
 
+    public int getSectionsCompleted() {
+        return sectionsCompleted;
+    }
 
+    public void setSectionsCompleted(int sectionsCompleted) {
+        this.sectionsCompleted = sectionsCompleted;
+    }
+
+    public ArrayList<ArrayList<double>> getChapters() {
+        return chapters;
+    }
+
+    public void setChapters(ArrayList<ArrayList<double>> chapters) {
+        this.chapters = chapters;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public Date getDateCompleted() {
+        return dateCompleted;
+    }
+
+    public void setDateCompleted(Date dateCompleted) {
+        this.dateCompleted = dateCompleted;
+    }
+
+    public String getCertificateId() {
+        return certificateId;
+    }
+
+    public void setCertificateId(String certificateId) {
+        this.certificateId = certificateId;
+    }
 
     public CourseProgress(Course course, int chaptersCompleted, int sectionsCompleted){
 
