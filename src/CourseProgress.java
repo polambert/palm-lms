33ca85b6e082786b6f6ed1fb7cc3b6c7a6febc54
Parker Lambert <contact@polambert.com>
2023-03-16 12:52:03 -0400

    updates to CourseProgress to get ready for data loading

--- src/CourseProgress.java
+++ src/CourseProgress.java
@@ -1,118 +1,111 @@
 
 import java.util.ArrayList;
-import java.util.UUID;
-import java.util.Date;
 
-public class CourseProgress 
-{
-    private Course course;
-    private User user;
-    private int chaptersCompleted;
-    private int sectionsCompleted;
-    private ArrayList<ArrayList<Double>> grades = new ArrayList<>();
-    private double grade;
-    private Date dateCompleted;
-    private String certificateId;
+import java.time.LocalDate;
+
+public class CourseProgress {
+	private Course course;
+	private int chaptersCompleted;
+	private int sectionsCompleted;
+	private ArrayList<ArrayList<Double>> grades = new ArrayList<>();
+	private double grade;
+	private LocalDate dateCompleted;
+	private String certificateId;
 
 
-    public CourseProgress(Course course, int chaptersCompleted, int sectionsCompleted) {
+	public CourseProgress(Course course, int chaptersCompleted, int sectionsCompleted, ArrayList<ArrayList<Double>> grades) {
+		this.course = course;
+		this.chaptersCompleted = chaptersCompleted;
+		this.sectionsCompleted = sectionsCompleted;
+		this.grades = grades;
+	}
 
-    }
-
-    public CourseProgress(Course course) {
-
-    }
+	public CourseProgress(Course course) {
+		this.course = course;
+	}
 
 
-    // getters and setters
-    public Course getCourse() {
-        return course;
-    }
+	// getters and setters
+	public Course getCourse() {
+		return course;
+	}
 
-    public void setCourse(Course course) {
-        this.course = course;
-    }
+	public void setCourse(Course course) {
+		this.course = course;
+	}
 
-    public User getUser() {
-        return user;
-    }
+	public int getChaptersCompleted() {
+		return chaptersCompleted;
+	}
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+	public void setChaptersCompleted(int chaptersCompleted) {
+		this.chaptersCompleted = chaptersCompleted;
+	}
 
-    public int getChaptersCompleted() {
-        return chaptersCompleted;
-    }
+	public int getSectionsCompleted() {
+		return sectionsCompleted;
+	}
 
-    public void setChaptersCompleted(int chaptersCompleted) {
-        this.chaptersCompleted = chaptersCompleted;
-    }
+	public void setSectionsCompleted(int sectionsCompleted) {
+		this.sectionsCompleted = sectionsCompleted;
+	}
 
-    public int getSectionsCompleted() {
-        return sectionsCompleted;
-    }
+	public ArrayList<ArrayList<Double>> getGrades() {
+		return grades;
+	}
 
-    public void setSectionsCompleted(int sectionsCompleted) {
-        this.sectionsCompleted = sectionsCompleted;
-    }
+	public void setgrades(ArrayList<ArrayList<Double>> grades) {
+		this.grades = grades;
+	}
 
-    public ArrayList<ArrayList<Double>> getGrades() {
-        return grades;
-    }
+	public double getGrade() {
+		return grade;
+	}
 
-    public void setgrades(ArrayList<ArrayList<Double>> grades) {
-        this.grades = grades;
-    }
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
 
-    public double getGrade() {
-        return grade;
-    }
+	public LocalDate getDateCompleted() {
+		return dateCompleted;
+	}
 
-    public void setGrade(double grade) {
-        this.grade = grade;
-    }
+	public void setDateCompleted(LocalDate dateCompleted) {
+		this.dateCompleted = dateCompleted;
+	}
 
-    public Date getDateCompleted() {
-        return dateCompleted;
-    }
+	public String getCertificateId() {
+		return certificateId;
+	}
 
-    public void setDateCompleted(Date dateCompleted) {
-        this.dateCompleted = dateCompleted;
-    }
+	public void setCertificateId(String certificateId) {
+		this.certificateId = certificateId;
+	}
 
-    public String getCertificateId() {
-        return certificateId;
-    }
+	public int getChapterProgress(){
+		return chaptersCompleted;
+	}
+	public int getSectionProgress(){
+		return sectionsCompleted;
+	}
+	public void setChapterProgress(int newChapterProgress) {
 
-    public void setCertificateId(String certificateId) {
-        this.certificateId = certificateId;
-    }
+	}
+	public void setSectionProgress(int newSectionProgress) {
 
-    public int getChapterProgress(){
-        return chaptersCompleted;
-    }
-    public int getSectionProgress(){
-        return sectionsCompleted;
-    }
-    public void setChapterProgress(int newChapterProgress) {
-
-    }
-    public void setSectionProgress(int newSectionProgress) {
-
-    }
-    public int incChapterProgress(){   
-        return chaptersCompleted+1;
-    }
-    public int incSectionProgress(){
-        return sectionsCompleted+1;
-    }
-    public double getProgressPercent(){
-        return chaptersCompleted/course.getChapterCount();
-    }
-    public String generateCertificate(){
-        return "";
-    }
+	}
+	public int incChapterProgress(){   
+		return chaptersCompleted+1;
+	}
+	public int incSectionProgress(){
+		return sectionsCompleted+1;
+	}
+	public double getProgressPercent(){
+		return chaptersCompleted/course.getChapterCount();
+	}
+	public String generateCertificate(){
+		return "";
+	}
 
 
 
